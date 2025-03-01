0936909cfabfb631169fc74e0af062310d52a6a5
taylorhelene <taylorhelene09@gmail.com>
2025-03-01 16:17:20 +0300

    server

--- Server.js
+++ Server.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const User = require('./User')
 
+const salt = bcrypt.genSaltSync(10);
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -12,3 +14,11 @@ app.use(cors());
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Register Route
+app.post('/register',async(req,res)=>{
+    const { email, password, role } = req.body;
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = new User({ email, password: hashedPassword, role });
+    await user.save();
+    res.status(201).json({ message: "User registered successfully!" });
+})
