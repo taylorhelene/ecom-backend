d3a741dc88e0a7375202e01c01759bcd705f337e
taylorhelene <taylorhelene09@gmail.com>
2025-03-01 15:31:58 +0300

    server

---
+++ User.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    email : {type : String },
+    name : String,
+    password : String,
+    role : { type: String, default: "user" } // "admin" or "user"
+})
+const User = mongoose.model("User", UserSchema);
+module.exports(User)
