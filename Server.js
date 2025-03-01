2a1a38246182c81abaacc037eed2767c55c4caff
taylorhelene <taylorhelene09@gmail.com>
2025-03-01 15:25:06 +0300

    server

---
+++ Server.js
@@ -0,0 +1,14 @@
+require('dotenv').config;
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
